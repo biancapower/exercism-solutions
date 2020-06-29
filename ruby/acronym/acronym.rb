588e8adf57bd2ec323693ef3472603c4962a43a3
biancapower <bianca@biancapower.com>
2020-06-30 07:45:42 +1000

    solve acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,8 +1,11 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
+class Acronym
+  def self.abbreviate(phrase)
+    acronym = ""
 
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
+    # match one or more characters that are not letters, use them to split string into an array
+    # loop through the array and add the first letter of each word to the acronym variable
+    phrase.split(/[^a-zA-Z]+/).each{ |word| acronym << word[0] }
 
+    acronym.upcase
+  end
+end
