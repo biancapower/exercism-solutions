390c34667990e60ce64cd6c7eb51e457bb1496d3
biancapower <bianca@biancapower.com>
2020-06-29 17:34:06 +1000

    solve microwave

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -7,8 +7,19 @@ class Microwave
   end
 
   def timer
-    minutes = @time / 60
-    seconds = @time % 60
+    if @time >= 100
+      minutes = @time / 100
+      seconds = @time % 100
+
+      if seconds >= 60
+        minutes = minutes + seconds / 60
+        seconds = seconds % 60
+      end
+
+    else
+      minutes = @time / 60
+      seconds = @time % 60
+    end
 
     display_time = format('%02d', minutes).to_s + ":" + format('%02d', seconds).to_s
   end
