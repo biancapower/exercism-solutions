9b40c9844a9923e075b5dfe10d4824893f533848
biancapower <bianca@biancapower.com>
2020-06-29 15:58:31 +1000

    refactor resistor color duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,9 +1,9 @@
 class ResistorColorDuo
-  def self.value(colours)
-    @resistors = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
+  RESISTORS = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
 
-    num_one = @resistors.index(colours[0].capitalize)
-    num_two = @resistors.index(colours[1].capitalize)
+  def self.value(colours)
+    num_one = RESISTORS.index(colours[0].capitalize)
+    num_two = RESISTORS.index(colours[1].capitalize)
 
     result = (num_one.to_s + num_two.to_s).to_i
   end
