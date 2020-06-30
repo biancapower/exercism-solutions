a86b3c13ddc6cdc4942b6a184ca9b579c5e9c71c
biancapower <bianca@biancapower.com>
2020-06-30 16:31:27 +1000

    improve acronym solution

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,11 +1,5 @@
 class Acronym
   def self.abbreviate(phrase)
-    acronym = ""
-
-    # match one or more characters that are not letters, use them to split string into an array
-    # loop through the array and add the first letter of each word to the acronym variable
-    phrase.split(/[^a-zA-Z]+/).each{ |word| acronym << word[0] }
-
-    acronym.upcase
+    phrase.scan(/\b[a-zA-Z]/).join.upcase
   end
 end
