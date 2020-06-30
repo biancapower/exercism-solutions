5b4485df2a199dd1241ec7720001a287ec02ebab
biancapower <bianca@biancapower.com>
2020-06-30 20:52:10 +1000

    pass first test

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,8 +1,13 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  def initialize(game_score)
+    @game_score = game_score
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  def new
+  end
+
+  def scores
+    @game_score
+  end
+end
 
