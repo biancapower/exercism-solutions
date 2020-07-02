2d1af7a2e9fd2fb9fb0b9055498055f4aea5c05f
biancapower <bianca@biancapower.com>
2020-07-02 21:03:21 +1000

    refactor

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,17 +1,19 @@
 class Matrix
+  attr_reader :numbers, :arr
+
   def initialize(numbers)
     @numbers = numbers
     @arr = []
   end
 
   def processed_input
-    all_rows = @numbers.split("\n")
+    all_rows = numbers.split("\n")
     
     all_rows.each do |r|
-      @arr << r.scan(/\b\d+/)
+      arr << r.scan(/\b\d+/)
     end
 
-    @arr.each do |sub_array|
+    arr.each do |sub_array|
       sub_array.map!(&:to_i)
     end
   end
