bba5c7a0ae4e898cb6bfdbcf7d6a87ce3b66ba18
biancapower <bianca@biancapower.com>
2020-07-02 20:35:47 +1000

    pass all rows tests

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,8 +1,25 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+#rows = nums.split("\n")
+#rows.each{|r| puts r.scan(/\b\d+/)}
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+class Matrix
+  def initialize(numbers)
+    @numbers = numbers
+  end
 
+  def rows
+    arr = []
+
+    all_rows = @numbers.split("\n")
+    
+    all_rows.each do |r|
+      arr << r.scan(/\b\d+/)
+    end
+
+    arr.each do |sub_array|
+      sub_array.map!(&:to_i)
+    end
+
+  end
+end
+
+p Matrix.new("1 2\n10 20").rows[0]
