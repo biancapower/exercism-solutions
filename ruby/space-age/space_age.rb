084ec89ddaabf49388e678e9212424e669488afe
biancapower <bianca@biancapower.com>
2020-07-09 22:46:59 +1000

    refactor space age to use define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -10,48 +10,27 @@ Neptune: orbital period 164.79132 Earth years
 So if you were told someone were 1,000,000,000 seconds old, you should be able to say that they're 31.69 Earth-years old.
 =end
 class SpaceAge
-  attr_accessor :age, :earth, :mercury, :venus, :mars, :jupiter, :saturn, :uranus, :neptune
+  attr_accessor :age, :earth
+  
   def initialize(age)
     @age = age
     @earth = age / 31557600.0
-    @mercury = earth / 0.2408467
-    @venus = earth / 0.61519726
-    @mars = earth / 1.8808158
-    @jupiter = earth / 11.862615
-    @saturn = earth / 29.447498
-    @uranus = earth / 84.016846
-    @neptune = earth / 164.79132
   end
 
-  def on_earth
-    earth
-  end
+  CONVERSIONS = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
-  def on_mercury
-    mercury
-  end
-
-  def on_venus
-    venus
-  end
-
-  def on_mars
-    mars
-  end
-
-  def on_jupiter
-    jupiter
-  end
-
-  def on_saturn
-    saturn
-  end
-
-  def on_uranus
-    uranus
-  end
-
-  def on_neptune
-    neptune
+  CONVERSIONS.each do |planet, conversion|
+    define_method("on_#{planet}") do
+      earth / conversion
+    end
   end
 end
