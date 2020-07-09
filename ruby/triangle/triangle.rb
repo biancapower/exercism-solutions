304e0acab1e7a230110a6aad419847b7dec58440
biancapower <bianca@biancapower.com>
2020-07-09 20:26:40 +1000

    triangle

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,8 +1,26 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+class Triangle
+  attr_accessor :side_lengths, :a, :b, :c
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+  def initialize(side_lengths)
+    @side_lengths = side_lengths.sort!
+    @a = side_lengths[0]
+    @b = side_lengths[1]
+    @c = side_lengths[2]
+  end
 
+  def valid?
+    !side_lengths.include?(0) && ((a + b) > c)
+  end
+
+  def equilateral?
+    valid? && a == b && a == c
+  end
+
+  def isosceles?
+    valid? && (a == b || a == c || b == c)
+  end
+
+  def scalene?
+    valid? && !equilateral? && !isosceles?
+  end
+end
