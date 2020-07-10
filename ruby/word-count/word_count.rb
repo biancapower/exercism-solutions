1abb8538d00d65d395b29cb63c0447f8d3ea402c
biancapower <bianca@biancapower.com>
2020-07-10 22:11:59 +1000

    word count passes basic tests

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  attr_accessor :phrase
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
+  def initialize(phrase)
+    @phrase = phrase
+  end
 
+  def word_count
+    words = phrase.split(' ')
+
+    counts = {}
+
+    words.each do |w|
+      counts.has_key?(w) ? counts[w] += 1 : counts[w] = 1
+    end
+
+    counts
+  end
+end
--- ruby/word-count/word_count_test.rb
+++ ruby/word-count/word_count_test.rb
@@ -11,14 +11,12 @@ class WordCountTest < Minitest::Test
   end
 
   def test_count_one_of_each_word
-    skip
     phrase = Phrase.new("one of each")
     counts = {"one"=>1, "of"=>1, "each"=>1}
     assert_equal counts, phrase.word_count
   end
 
   def test_multiple_occurrences_of_a_word
-    skip
     phrase = Phrase.new("one fish two fish red fish blue fish")
     counts = {"one"=>1, "fish"=>4, "two"=>1, "red"=>1, "blue"=>1}
     assert_equal counts, phrase.word_count
