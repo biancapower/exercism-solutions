1c90d966a3068aa9b8c6b883217fff2a91c3ab0a
biancapower <bianca@biancapower.com>
2020-08-27 20:53:54 +1000

    solve gigasecond

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,10 @@
 =begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
+Calculate the moment when someone has lived for 10^9 seconds.
+A gigasecond is 10^9 (1,000,000,000) seconds.
 =end
 
+class Gigasecond
+  def self.from(time)
+    time + 1000000000
+  end
+end
